bbcd2bc956c1a9d6826e35c24e521ec8ddeb8f8b
Ram Kishor <krishnaenterprises0423@gmail.com>
2024-05-17 09:03:24 +0530

    ok

--- src/main/java/org/unlimits/rest/crud/service/CrudServiceImpl.java
+++ src/main/java/org/unlimits/rest/crud/service/CrudServiceImpl.java
@@ -3,6 +3,7 @@
  */
 package org.unlimits.rest.crud.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.data.domain.Page;
@@ -19,18 +20,28 @@ public abstract class CrudServiceImpl<DT, EN, ID> implements CrudService<DT,EN, 
 
 	@Override
 	public DT add(DT data) {
+		preAdd(data);
 		EN mappedToDT = getMapper().mapToDAO(data);
 		EN save = getRepository().save(mappedToDT);
 		return getMapper().mapToDTO(save);
 	}
 
+	protected void preAdd(DT data) {
+		
+	}
+
 	@Override
 	public DT update(DT data) {
+		preUpdate(data);
 		EN mappedToDT = getMapper().mapToDAO(data);
 		EN save = getRepository().save(mappedToDT);
 		return getMapper().mapToDTO(save);
 	}
 	
+	protected void preUpdate(DT data) {
+		
+	}
+
 	@Override
 	public DT update(ID id, DT dto) {
 		DT findById = findById(id);
@@ -48,34 +59,58 @@ public abstract class CrudServiceImpl<DT, EN, ID> implements CrudService<DT,EN, 
 	
 	@Override
 	public DT find(ID uuid) {
-		return getMapper().mapToDTO(getRepository().getReferenceById(uuid));
+		EN findObject = getRepository().getReferenceById(uuid);
+		DT dtoObject = getMapper().mapToDTO(findObject);
+		postFind(findObject, dtoObject);
+		return dtoObject;
+	}
+
+	protected void postFind(EN findObject, DT dtoObject) {
+		
 	}
 
 	@Override
 	public DT findById(ID uuid) {
-		return getMapper().mapToDTO(getRepository().getReferenceById(uuid));
+		EN findObject = getRepository().getReferenceById(uuid);
+		DT dtoObject = getMapper().mapToDTO(findObject);
+		postFind(findObject, dtoObject);
+		return dtoObject;
 	}
 	
 	@Override
 	public List<DT> findAllById(List<ID> ids) {
-		return getMapper().mapToDTO(getRepository().findAllById(ids));
+		
+		List<EN> findObjects = getRepository().findAllById(ids);
+		return postCall(findObjects);
+	}
+
+	private List<DT> postCall(List<EN> findObjects) {
+		List<DT> list=new ArrayList<DT>();
+		for(EN findObject : findObjects) {
+			DT dtoObject = getMapper().mapToDTO(findObject);
+			list.add(dtoObject);
+			postFind(findObject, dtoObject);
+		}
+		return list;
 	}
 
 	@Override
 	public List<DT> findAll() {
-		return getMapper().mapToDTO(getRepository().findAll()) ;
+		List<EN> findObjects = getRepository().findAll();
+		return postCall(findObjects);
 	}
 	
 	@Override
 	public List<DT> findAll(Sort sort) {
-		return getMapper().mapToDTO(getRepository().findAll(sort)) ;
+		List<EN> findObjects = getRepository().findAll(sort);
+		return postCall(findObjects);
 	}
 
 	@Override
 	public PageDetail fetchPageObject(int pageNumber, int count) {
 		Pageable pageable =PageRequest.of(pageNumber, count);
 		Page<EN> page = getRepository().findAll(pageable);
-		List<DT> reslist = getMapper().mapToDTO(page.toList());
+		List<DT> reslist = postCall(page.toList());
 		PageDetail responseDto=new PageDetail();
 		responseDto.setCount(reslist.size());
 		responseDto.setElements(reslist);
@@ -86,7 +121,7 @@ public abstract class CrudServiceImpl<DT, EN, ID> implements CrudService<DT,EN, 
 	public PageDetail fetchPageObject(int pageNumber, int count, Sort sort) {
 		Pageable pageable =PageRequest.of(pageNumber, count , sort);
 		Page<EN> page = getRepository().findAll(pageable);
-		List<DT> reslist = getMapper().mapToDTO(page.toList());
+		List<DT> reslist = postCall(page.toList());
 		PageDetail responseDto=new PageDetail();
 		responseDto.setCount(reslist.size());
 		responseDto.setElements(reslist);
@@ -97,13 +132,13 @@ public abstract class CrudServiceImpl<DT, EN, ID> implements CrudService<DT,EN, 
 	public List<DT> fetchPageList(int pageNumber, int count) {
 		Pageable pageable =PageRequest.of(pageNumber, count);
 		Page<EN> page = getRepository().findAll(pageable);
-		return getMapper().mapToDTO(page.toList());
+		return postCall(page.toList());
 	}
 	
 	@Override
 	public List<DT> fetchPageList(int pageNumber, int count , Sort sort) {
 		Pageable pageable =PageRequest.of(pageNumber, count, sort);
 		Page<EN> page = getRepository().findAll(pageable);
-		return getMapper().mapToDTO(page.toList());
+		return postCall(page.toList());
 	}
 }
