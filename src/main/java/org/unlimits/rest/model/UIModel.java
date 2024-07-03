6b08c5fa8819e0ea1c721e1e9720eec02805b275
Ram Kishor <ramkishor0509@gmail.com>
2024-07-03 09:16:39 +0530

    OK

---
+++ src/main/java/org/unlimits/rest/model/UIModel.java
@@ -0,0 +1,29 @@
+package org.unlimits.rest.model;
+
+public class UIModel {
+	
+	private Long id;
+
+	private Double orderSequence;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Double getOrderSequence() {
+		if(orderSequence==null) {
+			orderSequence=0.0d;
+		}
+		return orderSequence;
+	}
+
+	public void setOrderSequence(Double orderSequence) {
+		this.orderSequence = orderSequence;
+	}
+	
+	
+}
